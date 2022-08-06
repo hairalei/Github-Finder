1e611d6a947d59a4e60215a132279a9a10fc9ed3
Haira Lei <darhaii13@gmail.com>
2022-08-06 22:26:22 +0800

    Finish error page

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,12 @@
-import React from 'react';
-import { GithubContext } from '../context/context';
-import styled from 'styled-components';
-import { MdBusiness, MdLocationOn, MdLink } from 'react-icons/md';
+import React from "react";
+import { GithubContext } from "../context/context";
+import styled from "styled-components";
+import { MdBusiness, MdLocationOn, MdLink } from "react-icons/md";
+
 const Card = () => {
   return <h2>card component</h2>;
 };
+
 const Wrapper = styled.article`
   background: var(--clr-white);
   padding: 1.5rem 2rem;
@@ -13,7 +15,7 @@ const Wrapper = styled.article`
   border-bottom-right-radius: var(--radius);
   position: relative;
   &::before {
-    content: 'user';
+    content: "user";
     position: absolute;
     top: 0;
     left: 0;
