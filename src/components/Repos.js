e1bb0f24fc9ba1b2ebe5c1eb2082da4e050d17a8
Haira Lei <darhaii13@gmail.com>
2022-08-07 17:18:34 +0800

    Add and style doughnut 2d chart for stargazers count in repos

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -6,33 +6,45 @@ import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from "./Charts";
 const Repos = () => {
   const { repos } = useGlobalContext();
 
-  let languages = repos.reduce((total, item) => {
-    const { language } = item;
+  const languages = repos.reduce((total, item) => {
+    const { language, stargazers_count } = item;
 
     if (!language) return total;
 
     if (!total[language]) {
-      total[language] = { label: language, value: 1 };
+      total[language] = { label: language, value: 1, stars: stargazers_count };
     } else {
       total[language] = {
         ...total[language],
         value: total[language].value + 1,
+        stars: total[language].stars + stargazers_count,
       };
     }
 
     return total;
   }, {});
 
-  languages = Object.values(languages)
+  const mostUsed = Object.values(languages)
     .sort((a, b) => {
       return b.value - a.value;
+    })
+    .slice(0, 5);
+
+  //Most stars per language
+  const mostPopular = Object.values(languages)
+    .sort((a, b) => {
+      return b.stars - a.stars;
+    })
+    .map((item) => {
+      return { ...item, value: item.stars };
     })
     .slice(0, 5);
 
   return (
     <section className="section">
       <Wrapper>
-        <Pie3D data={languages} />
+        <Pie3D data={mostUsed} />
+        <Doughnut2D data={mostPopular} />
       </Wrapper>
     </section>
   );
